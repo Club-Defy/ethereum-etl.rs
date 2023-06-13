c56909fcf3a38304e7c05538fe6eede1f65cdbef
Gargi <gargi@roverx.team>
2023-06-13 18:43:48 +0530

    modified provider to return default provider

--- src/provider/get_provider.rs
+++ src/provider/get_provider.rs
@@ -2,12 +2,13 @@ use std::env;
 use web3::futures::TryFutureExt;
 
 pub async fn get_provider() -> &'static str {
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
 
-    let mut user_provider = if args.len() > 1 && !args[1].is_empty() {
-          args[1].get(()).expect("Using default provider...").take()
-    } ;
+    // let mut user_provider = if args.len() > 1 && !args[1].is_empty() {
+    //       args[1].get("provider").expect("Using default provider...").take()
+    // } ;
 
     "https://mainnet.infura.io/v3/9d205ba5694a43628ed84f7437319da7"
+
 }
 
